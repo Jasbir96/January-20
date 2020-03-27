d467835ba63c8229d0aea317a5c31b0150216d6a
Jasbir <mr.jasbirsingh96@gmail.com>
2020-03-27 13:51:43 +0530

    added upto sticky section

--- Lectures/FoodApplication_Frontend/Fair/js/script.js
+++ Lectures/FoodApplication_Frontend/Fair/js/script.js
@@ -1,2 +1,11 @@
-const span=document.querySelector(".span-detector");
-span.textContent
+const nav = document.querySelector("nav");
+const features = document.querySelector(".features");
+
+window.addEventListener("scroll", function() {
+  let pos = features.getBoundingClientRect().top;
+  if (pos < 0) {
+    nav.setAttribute("class", "sticky");
+  }else{
+    nav.removeAttribute("class","sticky");
+  }
+});
